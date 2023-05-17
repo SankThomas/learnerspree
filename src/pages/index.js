bec7cca3083d0f23e1076aec00ed6ff3ff943d1c
Thomas Sankara <thomassankara74@gmail.com>
2023-05-17 23:08:38 +0300

    add dark mode support

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,17 +14,17 @@ export default function Home() {
       </Head>
       <section className="flex items-center justify-center h-screen">
         <div className="max-w-3xl mx-auto px-8 text-center">
-          <h1 className="text-4xl lg:text-5xl 2xl:text-6xl mb-8 text-[#111111] dark:text-white font-bold uppercase tracking-widest">
+          <h1 className="text-4xl lg:text-5xl 2xl:text-6xl mb-8 font-bold uppercase tracking-widest">
             Eduketa
           </h1>
-          <p className="text-[#333333] dark:text-white opacity-75 leading-relaxed mb-8">
+          <p className="landing-text opacity-75 leading-relaxed mb-8">
             Eduketa is an online educational platform that allows you access to
             programming resources, and quizes and projects that will help you
             build real world projects.
           </p>
           <Link
             href="/home"
-            className="bg-[#111111] dark:bg-white py-3 px-6 rounded text-[#ffffff] dark:text-black shadow"
+            className="landing-button py-3 px-6 rounded shadow"
           >
             Get Started
           </Link>
