50c33bf5c7f3d3e0ad5a314e2037923527c2b173
Thomas Sankara <thomassankara74@gmail.com>
2023-05-24 23:22:48 +0300

    update homepage styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,8 @@ export default function Home() {
           build real world projects.
         </title>
       </Head>
-      <section className="flex items-center justify-center h-screen">
+
+      <section className="flex items-center justify-center h-screen bg-[#111111]">
         <div className="max-w-3xl mx-auto px-8 text-center">
           <h1 className="text-4xl lg:text-5xl 2xl:text-6xl mb-8 font-bold uppercase tracking-widest">
             Eduketa
@@ -22,7 +23,10 @@ export default function Home() {
             programming resources, and quizes and projects that will help you
             build real world projects.
           </p>
-          <Link href="/home" className="py-3 px-6 rounded shadow">
+          <Link
+            href="/home"
+            className="py-3 px-6 rounded shadow bg-blue-500 text-white hover:bg-blue-600 transition-all duration-150 inline-block mt-2"
+          >
             Get Started
           </Link>
         </div>
