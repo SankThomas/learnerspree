1be8f20bdfd5c593dd299d3f6fce71598ce00f06
SankThomas <thomassankara74@gmail.com>
2025-11-01 14:51:49 +0300

    add nextra config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+import nextra from "nextra";
+
+const withNextra = nextra({
+  search: { codeblocks: false },
+});
+
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
 };
 
-export default nextConfig;
+export default withNextra(nextConfig);
